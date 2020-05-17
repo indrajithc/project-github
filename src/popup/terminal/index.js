ffc20610837aec9ef46c184565c125b6ac83973d
Indrajith C <indranspeaks@gmail.com>
2020-05-18 02:37:41 +0530

    hisotry arrow key events added

--- src/popup/terminal/index.js
+++ src/popup/terminal/index.js
@@ -18,8 +18,17 @@ $(() => {
   }  
 
   const doc = $(document);
+  const readFile = window.readFile || null;
+  const storageSet = window.storageSet;
+  const storageGet = window.storageGet;
+
+
   let defaultText = `[user@git ~]$ `;
+  const defaultTextRegx = new RegExp("\[[a-zA-Z0-9 ._-]+@[a-zA-Z0-9 ._-]+\\~\\]\\$");
   let lastInput = defaultText;
+  const localHistory = [];
+  let currentHistoryIndex = 0;
+
   /** ====== define global values ===== */
   const formGotoLogin = $("#goto-login");
 
@@ -50,6 +59,7 @@ $(() => {
       $("body").removeAttr("unauthorized");
       if( payload.userId) {
         defaultText = defaultText.replace("user", payload.userId);
+        setInputToDefault();
       }
     } else {
       $("body").attr("unauthorized");
@@ -73,6 +83,10 @@ $(() => {
     }
   }
 
+  const allEventMonitor = ()=> {
+    currentHistoryIndex = 0;
+  }
+
   const scrollToBottom = () => {
     setTimeout( o => {terminal[0].scrollTop = terminal[0].scrollHeight;}, 0);
   }
@@ -86,6 +100,7 @@ $(() => {
       event.preventDefault();
       inputResize('20px');
       }
+      allEventMonitor();
   }
 
   terminalConsole.click( (event) => {
@@ -99,17 +114,94 @@ $(() => {
   const setOutPut = (output) => {
     const eachActions = `<p class="d-block w-100 m-0 p-0">${output}</p>`;
     consoleOutput.append( eachActions);
+    scrollToBottom();
   }
 
+  const showHelp = () => {
+    readFile("resource/help.json").then( (response, error) => {
+      if(error) {
+        setOutPut("resource file missing");
+      } else  {
+        const parsedJson = JSON.parse( response);
+        let helpOnScreen = '<table><tbody>';
+        Object.keys(parsedJson).map( key => {
+           helpOnScreen = `${helpOnScreen}
+           <tr>
+           <td>${key}</td><td>${parsedJson[key]}</td>
+           </tr>`
+        });
+        helpOnScreen=`${helpOnScreen}</tbody></table>`;
+        setOutPut(helpOnScreen);
+      } 
+    });
+  }
+
+  const clearHistory = (  ) => {       
+      storageSet( { history : [] },()=> {
+        setOutPut("history cleared.");
+    }); 
+}
+
+  const setHistory = ( command ) => {
+    localHistory.push(command);
+     storageGet(['history'], (response)=> {
+       let updatedHistory = [];
+       if( Array.isArray(response.history)) {
+        updatedHistory= response.history;
+      }
+      updatedHistory.push(command); 
+      storageSet( { history : updatedHistory },()=> {
+        // console.log("history updated"); 
+    });
+
+  })
+}
+
+  const showHistory  = () => {
+    storageGet(['history'], (response)=> {
+      const history = response.history
+      if( Array.isArray(history)) {
+        for( let o =0 ; o<history.length ; o++) {
+          setOutPut(`<span class="pl-1 d-flex">
+            <span style="min-width: 2rem;" class="pr-2 text-right">${(o+1)}</span>
+            <span class="d-block">${history[o]}</span>
+          </span>
+          `);
+        }
+      }
+    });
+  }
+
+  const showHistoryInInput = ( direction)=> {
+    currentHistoryIndex += direction;
+    if( currentHistoryIndex <= localHistory.length){
+      const selectedHistory = localHistory[ localHistory.length - currentHistoryIndex ];
+      if(selectedHistory)
+        consoleInput.val(`${defaultText}${selectedHistory}`);
+    }
+  }
+
+
   const doTheAction = ( command ) => {
-    command = command.replace(defaultText.trim(), "");
+    command = command.replace(defaultTextRegx, "");
     command = command.trim();
+    command = command.replace(/\s\s+/g, ' ');
     if(command.length < 1 ) return;
+    setHistory(command);
     let output ="";
     switch( command){
       case "clear" : 
         consoleOutput.html("");
+        case "help" : 
+          showHelp();
         break;
+        case "history" : 
+        showHistory();
+        break;
+        case "history -c":
+        case "history --clear":
+          clearHistory();
+          break;
       default : 
         output = `git: ${command}: command not found`
     }
@@ -129,14 +221,17 @@ $(() => {
     event.preventDefault();
     const inLast = lastInput.replace(defaultText, "");
     let currentInput = consoleInput.val();
-    if( defaultText !== currentInput.substring(0, defaultText.length)) {
+    if(currentInput.substring(0, defaultText.length).match(defaultText)) {
       if(currentInput.length < 1) {
         currentInput = defaultText;
       } else  {
         currentInput = defaultText+inLast;
-
       }
+    }else  if(currentInput.length <= defaultText.length)  {
+      currentInput = defaultText;
     }
+    
+    allEventMonitor();
     consoleInput.val( currentInput);
     window.setTimeout(inputResize, 0);
     scrollToBottom();
@@ -151,6 +246,21 @@ $(() => {
       enterMonitor(event);
     }
   });
+
+  consoleInput.keydown((event)=> {
+    if(event ) { 
+       if( event.which === 37 || event.which === 40) {
+          showHistoryInInput( -1);
+          event.preventDefault();
+       } else if( event.which === 38 || event.which === 39)  {
+          showHistoryInInput( +1);
+          event.preventDefault();
+       }
+    }
+  });
+
+  
+
  
 
 
